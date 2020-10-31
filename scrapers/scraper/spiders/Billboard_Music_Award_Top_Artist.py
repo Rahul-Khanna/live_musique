a95d35033acabf41fcb4caacbeea864952613926
Rahul Khanna <rkhanna712@gmail.com>
2020-10-30 23:57:28 -0700

    Billboard award crawlers and some useful functions for dealing with Wikipedia tables

--- scrapers/scraper/spiders/Billboard_Music_Award_Top_Artist.py
+++ scrapers/scraper/spiders/Billboard_Music_Award_Top_Artist.py
@@ -1,11 +1,14 @@
-# scrapy crawl Billboard_Music_Award -o ./output/Billboard_Music_Award.jl -t jsonlines
+# scrapy crawl Billboard_Music_Award_Artists -o ./output/Billboard_Music_Award_Artist.jl -t jsonlines
 import scrapy
-import re
 from bs4 import BeautifulSoup
+import sys
+sys.path.append("scraper/spiders/")
+import utils
 
 
-class Billboard_Music_Award_Scraper(scrapy.Spider):
-    name = "Billboard_Music_Award"
+class Billboard_Music_Award_Artists_Scraper(scrapy.Spider):
+    name = "Billboard_Music_Award_Artists"
+    award_org = "Billboard_Music_Award"
     start_urls = ['https://en.wikipedia.org/wiki/Billboard_Music_Award_for_Top_Male_Artist',
                   'https://en.wikipedia.org/wiki/Billboard_Music_Award_for_Top_Female_Artist',
                   'https://en.wikipedia.org/wiki/Billboard_Music_Award_for_Top_Artist']
@@ -13,20 +16,18 @@ class Billboard_Music_Award_Scraper(scrapy.Spider):
     download_delay = 1.25
     pages_processed = 0
 
-    def _create_key(self, year, award, org_name="Billboard_Music_Award"):
-        return "-".join([org_name, award, year])
-
     def _flatten_row(self, winner, winner_link, nominated_artists, year, award="Top Male Artist"):
         flattened_row = []
-        key = self._create_key(str(year), award)
+        key = utils.create_award_key(self.award_org, award, str(year))
         winner_output = {
             "key" : key,
-            "award_organization" : self.name,
+            "award_organization" : self.award_org,
             "award_classification" : award,
             "year" : year,
             "artists" : [winner],
-            "links" : [winner_link],
+            "artist_links" : [winner_link],
             "creative_work" : "",
+            "creative_work_link" : "",
             "is_winner" : True
         }
 
@@ -34,12 +35,13 @@ class Billboard_Music_Award_Scraper(scrapy.Spider):
         for artist in nominated_artists:
             output = {
                 "key" : key,
-                "award_organization" : self.name,
+                "award_organization" : self.award_org,
                 "award_classification" : award,
                 "year" : year,
                 "artists" : [artist["artist"]],
-                "links" : [artist["link"]],
+                "artist_links" : [artist["link"]],
                 "creative_work" : "",
+                "creative_work_link" : "",
                 "is_winner" : False
             }
 
@@ -54,16 +56,16 @@ class Billboard_Music_Award_Scraper(scrapy.Spider):
         soup_html = BeautifulSoup(response.text, 'lxml')
         tables = soup_html.findAll("table", {"class": "wikitable"})
         if 'Female' in url or "Top_Artist" in url:
-            award_classification = "Top Female Artist" if "Female" in url else "Top Artist"
+            award_classification = "Top_Female_Artist" if "Female" in url else "Top_Artist"
             tables = tables[:4]
             for table in tables:
                 tds = table.findAll("td")
                 raw_table_data = []
                 for td in tds:
-                    text = td.text.strip()
+                    text = utils.get_text(td)
                     possible_link = td.find('a', href=True)
                     if possible_link:
-                        link = possible_link["href"]
+                        link = utils.get_link(possible_link)
                     else:
                         link = ""
                     
@@ -81,12 +83,13 @@ class Billboard_Music_Award_Scraper(scrapy.Spider):
                     if text.isnumeric():
                         curr_year = int(text)
                     elif "[" not in text and text != "N/A":
-                        award_organization = self.name
+                        award_organization = self.award_org
                         year = curr_year
-                        key = self._create_key(str(year), award_classification)
+                        key = utils.create_award_key(self.award_org, award_classification, str(year))
                         artists = [text]
                         links = [entry["link"]]
                         creative_work = ""
+                        creative_work_link = ""
                         is_winner = entry["is_winner"]
                         yield {
                                 "key" : key,
@@ -94,44 +97,37 @@ class Billboard_Music_Award_Scraper(scrapy.Spider):
                                 "award_classification" : award_classification,
                                 "year" : year,
                                 "artists" : artists,
-                                "links" : links,
+                                "artist_links" : links,
                                 "creative_work" : creative_work,
+                                "creative_work_link" : creative_work_link,
                                 "is_winner" : is_winner
                             }
         if "Male" in url:
-            award_classification = "Top Male Artist"
+            award_classification = "Top_Male_Artist"
             table = tables[0]
             rows = table.findAll("tr")
             output_data = []
             for row in rows:
                 tds = row.findAll("td")
                 if len(tds) == 3:
-                    year = tds[0].text.strip()
+                    year = utils.get_text(tds[0])
                     if year.isnumeric():
                         year = int(year)
 
-                    winner = tds[1].text.split("[")[0]
+                    winner = utils.get_text(tds[1]).split("[")[0]
                     possible_link = tds[1].find("a", href=True)
                     if possible_link:
-                        link = possible_link["href"]
+                        link = utils.get_link(possible_link)
                     else:
                         link = ""
 
                     nominated_artists = []
                     posible_artists = tds[2].findAll("a", text=True)
                     for artist in posible_artists:
-                        if "text" in artist:
-                            artist_name = artist["text"]
-                        else:
-                            artist_name = artist.get_text()
-                        
-                        artist_name = re.sub('\[\d+\]', '', artist_name)
+                        artist_name = utils.clean_name_of_footnote(utils.get_text(artist))                        
                         if len(artist_name):
                             artist_name = artist_name.split("[")[0]
-                            if "href" in artist.attrs:
-                                nominated_link = artist.attrs["href"]
-                            else:
-                                nominated_link = ""
+                            nominated_link = utils.get_link(artist)
                             nominated_artists.append({"artist" : artist_name, "link" : nominated_link})
                     
                     if len(posible_artists) < 2 and len(posible_artists) > 0:
