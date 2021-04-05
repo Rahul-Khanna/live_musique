f00ac6634bfe970724e81b3ca5c68fb8f8d60e5a
Rahul Khanna <rkhanna712@gmail.com>
2021-04-04 22:56:56 -0700

    clean up done on local machine

--- Rahul_Folder/training/GraphEmbeddingEncoder.py
+++ Rahul_Folder/training/GraphEmbeddingEncoder.py
@@ -2,25 +2,31 @@ import torch
 import torch.nn as nn
 
 class GraphEmbeddingEncoder(nn.Module):
-    def __init__(self, emb_weight, emb_dim=500, compressed_dim=400, final_dim=100, embedding_dropout_pct=0.2):
+    def __init__(self, emb_weight, emb_dim=500, compressed_dim=400, final_dim=50, embedding_dropout_pct=0.5):
         self.embedding_dropout_pct = embedding_dropout_pct
         self.emb_dim = emb_dim
         self.compressed_dim = compressed_dim
         self.final_dim = final_dim
+        
+        # Loading DistMult Embeddings
+        self.embeddings = nn.Embedding.from_pretrained(emb_weight, freeze=False)
 
+        # Dropout Layer
         self.embedding_dropout = nn.Dropout(p=self.embedding_dropout_pct)
         
-        self.embeddings = nn.Embedding.from_pretrained(emb_weight, freeze=False)
-        
+        # Feature Weight Matrix
         diagonal_vector = torch.zeros(self.emb_dim, 1)
         nn.init.xavier_uniform_(diagonal_vector)
         diagonal_vector = diagonal_vector.squeeze(1)
         self.feature_weight_matrix = nn.Parameter(torch.diag(input=diagonal_vector), requires_grad=True)
         
+        # Intermediate Compression
         self.compressing_projection = nn.Linear(self.emb_dim, self.compressed_dim)
 
+        # Non-Linear Activation
         self.relu =  nn.ReLU()
-      
+        
+        # Final Compression
         self.output_projection = nn.Linear(self.compressed_dim, self.final_dim)
 
     def forward(self, input_ids):
